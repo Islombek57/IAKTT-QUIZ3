327c507c8f3993e6c41de8b9e454d08034059c4e
Islombek57 <152577325+Islombek57@users.noreply.github.com>
2023-12-01 08:23:22 +0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+const questions = [
+    {
+        numb: 1,
+        question: "Noutbuklar va cho'ntak komputerlarida foydalaniladigan monitor turi qaysi?",
+        answer: "C. Suyuq kristalli monitorlar",
+        options: [
+            "A. Katod nurli trubkali monitorlar",
+            "B. Sensorli monitorlar",
+            "C. Suyuq kristalli monitorlar",
+            "D. Oddiy trubkali monitorlar",
+        ]
+    },
+    {
+        numb: 2,
+        question: "Qaysi printer turi maxsus baraban yordamida sifatli va tez chop qila oladi?",
+        answer: "A. Lazerli printer",
+        options: [
+            "A. Lazerli printer",
+            "B. Purkovichli printer",
+            "C. Termal printer",
+            "D. Matritsali printer",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Plotter nima?",
+        answer: "D. Katta hajmdagi chizmalarni chop qilishga mo'ljallangan qurilma",
+        options: [
+            "A. Simvolli va grafikli axborotlarni namoyish etish qurilmasi",
+            "B. Malumotlarni yirik o'lchamda tasvirlash uchun ishlatiladigan qurilma",
+            "C. Ma'lumot kiritish qurilmasi",
+            "D. Katta hajmdagi chizmalarni chop qilishga mo'ljallangan qurilma",
+        ]
+    },
+    {
+        numb: 4,
+        question: "Quyidagilarning qaysi biri operatsion tizimning asosiy vazifalariga kirmaydi?",
+        answer: "C. Qurilmaga bo'lgan kiber hujumni oldini olish",
+        options: [
+            "A. Fayl tizimini boshqarish",
+            "B. Foydalanuvchi bilan muloqot",
+            "C. Qurilmaga bo'lgan kiber hujumni oldini olish",
+            "D. Erkin foydalanishni nazorat qilish",
+        ]
+    },
+    {
+        numb: 5,
+        question: "...... - masalni yechishning komputerdagi dasturiy amalga oshirilishi?",
+        answer: "B. Ilova",
+        options: [
+            "A. Dastur",
+            "B. Ilova",
+            "C. Dasturiy mahsulot",
+            "D. Dasturiy ta'minot",
+        ]
+    },
+    {
+        numb: 6,
+        question: "Microsoft Excel dasturida matnlar shirfini o'zgartirish qaysi bo'limda joylashgan?",
+        answer: "D. Asboblar paneli",
+        options: [
+            "A. Tezkor panel",
+            "B. Bosh menyu",
+            "C. Formula satri",
+            "D. Asboblar paneli",
+        ]
+    },
+    {
+        numb: 7,
+        question: "...... - Internet tarmog'ida joylashtirilgan axborot resurslarining murojaat manzillari",
+        answer: "A. Internet manzil",
+        options: [
+            "A. Internet manzil",
+            "B. Internet provayder",
+            "C. Proxy",
+            "D. Veb server",
+        ]
+    },
+    {
+        numb: 8,
+        question: "http: // ushbu belgi nimani anglatadi?",
+        answer: "D. protokol",
+        options: [
+            "A. sayt manzili",
+            "B. katalog",
+            "C. fayl nomi",
+            "D. protokol",
+        ]
+    },
+    {
+        numb: 9,
+        question: "Davlat va xukumat organlarini boshqarish vazifasini samarali bajarlishi qaysi davlat Elektron hukumatining tavsifi?",
+        answer: "B. Koreya",
+        options: [
+            "A. O'zbekiston",
+            "B. Koreya",
+            "C. AQSH",
+            "D. Rossiya",
+        ]
+    },
+    {
+        numb: 10,
+        question: "Algoritmning nechta turi bor?",
+        answer: "B. 3",
+        options: [
+            "A. 2",
+            "B. 3",
+            "C. 4",
+            "D. 5",
+        ]
+    }
+]
